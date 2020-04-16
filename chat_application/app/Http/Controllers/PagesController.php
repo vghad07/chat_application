c612fc67aeca18103a23f83a32dec7a8cd762b45
unknown <owais.khalilahmed@gmail.com>
2020-04-16 13:19:14 +0530

    chat application in Laravel

---
+++ chat_application/app/Http/Controllers/PagesController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    //
+    
+    public function index() {
+        $title = "Welcome to the index page of Laravel";
+      //  return view('pages.index',compact('title'));
+          return view('pages.index')->with('title',$title);
+    }
+
+    public function about() {
+        $title = "Welcome to about us page of Laravel project";
+        return view('pages.about')->with('title',$title);
+    }
+
+    public function service() {
+        $data = array(
+            'title'=>'Services page of Laravel project',
+            'services'=>['Web designing','Programming','Testing']
+        );
+        return view('pages.services')->with($data);
+    }
+}
+
+   
---
+++ chat_application/resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+     
+       @extends('layouts.app')
+       @section('content')
+       
+    <h1>{{$title}}</h1>
+       <h2>This is a About page </h2>
+  @endsection
