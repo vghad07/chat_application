d3a3e12dd297c47835cfb74506425a8153e2cb9d
vghad07 <63497489+vghad07@users.noreply.github.com>
2020-05-07 10:23:36 +0530

    update php-start.sh

--- php-start.sh
+++ php-start.sh
@@ -1,11 +1,9 @@
 ---
-#!/bin/bash
+#! /bin/bash
 
-echo "Allocate memory limit to composer"
-php -d memory_limit=-1 /usr/bin/composer install
+echo "Start Docker Container"
+docker exec -it chat_app /bin/bash
 
-echo "php site in live mode"
-php artisan up
+echo "php application running on port - localhost:9000"
+php artisan serve --host:0.0.0.0 --port=9000
 
-echo "php application running on port - localhost:8080"
-php artisan serve --host:0.0.0.0 --port=8080
