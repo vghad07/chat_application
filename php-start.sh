8188ee9b286361b2b6fd540f4b816e086655f0e9
vghad07 <63497489+vghad07@users.noreply.github.com>
2020-05-07 10:45:06 +0530

    Update php-start.sh

--- php-start.sh
+++ php-start.sh
@@ -2,7 +2,7 @@
 #! /bin/bash
 
 echo "Start Docker Container"
-docker exec -it chat_app /bin/bash
+docker exec -it chat_app
 
 echo "php application running on port - localhost:9000"
 php artisan serve --host:0.0.0.0 --port=9000
