9787313098a1d63c365a3e9fbb235cda45abaf02
vghad07 <63497489+vghad07@users.noreply.github.com>
2020-05-08 17:03:36 +0530

    Update php-start.sh

--- php-start.sh
+++ php-start.sh
@@ -3,6 +3,8 @@
 
 echo "Start Docker Container"
 docker exec --tty chat_app /bin/bash
-
+php -d memory_limit=-1 /usr/bin/composer install
+php artisan optimize:clear
+php artisan migrate
 php artisan serve --host=0.0.0.0 --port=9000
 
