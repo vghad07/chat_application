2027acc2b79ee92584ada375e7abc3ea6a804759
vghad07 <63497489+vghad07@users.noreply.github.com>
2020-05-09 15:25:46 +0530

    Update php-start.sh

--- php-start.sh
+++ php-start.sh
@@ -6,6 +6,3 @@ php -d memory_limit=-1 /usr/bin/composer install
 
 echo "php site in live mode"
 php artisan up
-
-echo "php application running on port - localhost:8080"
-php artisan serve --host:0.0.0.0 --port=8080
