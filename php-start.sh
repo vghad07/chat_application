93ed525e667f08cea6fb3a59d532740e67a295e2
vghad07 <63497489+vghad07@users.noreply.github.com>
2020-05-09 16:06:24 +0530

    Update php-start.sh

--- php-start.sh
+++ php-start.sh
@@ -2,7 +2,7 @@
 #! /bin/bash
 
 echo "Start Docker Container"
-docker exec --tty chat_app /bin/bash
+docker exec -i --tty chat_app /bin/bash
 echo "composer install"
 php -d memory_limit=-1 /usr/bin/composer install
 echo "Clear cache memory"
